29472e6d0be3b72a3d300cf2a6edbaf394b49462
Noel Vock <noel1uk@gmail.com>
2018-03-21 21:35:18 +0000

    Add soundtrack.

--- play.js
+++ play.js
@@ -304,6 +304,34 @@
 
       player = new createjs.BitmapAnimation(playerSheet); //creates a new player from the image
     }
+    var myGamePiece;
+
+
+    function sound(src) {
+        this.sound = document.createElement("audio");
+        this.sound.src = src;
+        this.sound.setAttribute("preload", "auto");
+        this.sound.setAttribute("controls", "none");
+        this.sound.style.display = "none";
+        document.body.appendChild(this.sound);
+        this.play = function(){
+            this.sound.play();
+        }
+        this.stop = function(){
+            this.sound.pause();
+        }
+    }
+
+var myMusic;
+
+function startGame() {
+    // myGamePiece = new component(30, 30, "red", 10, 120);
+    // mySound = new sound("bounce.mp3");
+    myMusic = new sound("music/streetfighter2turbo.mp3");
+    myMusic.play();
+    // myGameArea.start();
+}
+startGame();
     init();
 
 }());
