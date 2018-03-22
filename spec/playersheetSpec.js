f4b5a1e8d1317192258d8caacd00f33c6db5cc4a
Laura Chan <laurachan205@gmail.com>
2018-03-22 11:54:34 +0000

    Update Jasmine testing framework to 2.6.1

--- spec/playersheetSpec.js
+++ spec/playersheetSpec.js
@@ -1,5 +1,7 @@
 describe("Playersheet", function(){
 
+  var tileSheet
+
   it("playerSheet is a SpriteSheet instance", function(){
     expect(playerSheet).toEqual(jasmine.any(createjs.SpriteSheet))
   })
@@ -8,10 +10,14 @@ describe("Playersheet", function(){
     expect(player).toEqual(jasmine.any(createjs.BitmapAnimation))
   })
 
-  describe("addPlayer", function(){
-
-    it("sets the player's position on the map", function(){
-
-    })
+  it("whichColumn returns the player's location", function(){
+    spyOn(player, 'x').and.returnValue(48)
+    spyOn(player, 'width').and.returnValue(34)
+    spyOn(tileSheet, '_frameWidth').and.returnValue(48)
+    expect(whichColumn(player)).toEqual(1)
   })
+
+
+
+
 })
