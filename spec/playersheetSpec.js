2004ef48bff61cccb7d6c092e51b98d683f70d6b
Laura Chan <laurachan205@gmail.com>
2018-03-22 19:48:48 +0000

    Write tests

--- spec/playersheetSpec.js
+++ spec/playersheetSpec.js
@@ -1,7 +1,5 @@
 describe("Playersheet", function(){
 
-  var tileSheet
-
   it("playerSheet is a SpriteSheet instance", function(){
     expect(playerSheet).toEqual(jasmine.any(createjs.SpriteSheet))
   })
@@ -10,15 +8,29 @@ describe("Playersheet", function(){
     expect(player).toEqual(jasmine.any(createjs.BitmapAnimation))
   })
 
-  it("whichColumn returns the player's location", function(){
-    // spyOn(player, 'x').and.returnValue(48)
-    // spyOn(player, 'width').and.returnValue(34)
-    // spyOn(tileSheet, '_frameWidth').and.returnValue(48)
-    spyOnProperty(player, 'width', 'set').and.returnValue(34)
-    expect(player.width).toBe(34)
+  describe("addPlayer", function(){
+    beforeEach(function(){
+      var board = jasmine.createSpyObj('board', ['addChild'])
+      spyOn(tileSheet, '_frameWidth').and.returnValue(2)
+      spyOn(tileSheet, '_frameHeight').and.returnValue(2)
+      addPlayer(board, player, 1, 1, 1)
+    })
+
+    xit('sets the player x attribute', function(){
+
+    })
+
+    it('adds the player to the board', function(){
+      expect(board.addChild).toHaveBeenCalledWith(player)
+    })
   })
 
-
-
+  // xit("whichColumn returns the player's location", function(){
+  //   // spyOn(player, 'x').and.returnValue(48)
+  //   // spyOn(player, 'width').and.returnValue(34)
+  //   // spyOn(tileSheet, '_frameWidth').and.returnValue(48)
+  //   spyOnProperty(player, 'width', 'set').and.returnValue(34)
+  //   expect(player.width).toBe(34)
+  // })
 
 })
