2d20630bb980f42b60ae989993d9a075803e0462
Laura Chan <laurachan205@gmail.com>
2018-03-23 14:26:12 +0000

    Add spacing in playSpec.js

--- spec/playSpec.js
+++ spec/playSpec.js
@@ -26,33 +26,41 @@ describe('Play', function() {
   it('startMusic is defined', function(){
     expect(startMusic).toBeDefined()
   })
+
   it('dog is defined', function(){
     expect(dog).toBeDefined()
   })
+
   it('dogSheet is defined', function(){
     expect(dogSheet).toBeDefined()
   })
+
   it('addDog is defined', function(){
     expect(addDog).toBeDefined()
   })
+
   it('moveDog is defined', function(){
     expect(moveDog).toBeDefined()
   })
+
   it('wolf is defined', function(){
     expect(wolf).toBeDefined()
   })
+
   it('wolfSheet is defined', function(){
     expect(wolfSheet).toBeDefined()
   })
+
   it('addWolf is defined', function(){
     expect(addWolf).toBeDefined()
   })
+
   it('addWolf is defined', function(){
     expect(addWolf).toBeDefined()
   })
+
   it('wolfDeath is defined', function(){
     expect(wolfDeath).toBeDefined()
   })
-
-
+  
 })
