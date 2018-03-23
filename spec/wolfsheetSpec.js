5faa31d85b40a8b265f97b597f815b5d1bc345a3
Laura Chan <laurachan205@gmail.com>
2018-03-23 14:23:47 +0000

    Add tests to wolfSheetspec.js

--- spec/wolfsheetSpec.js
+++ spec/wolfsheetSpec.js
@@ -6,4 +6,51 @@ describe("wolfsheet", function(){
   it("wolf is a BitmapAnimation instance", function(){
     expect(wolf).toEqual(jasmine.any(createjs.BitmapAnimation))
   })
+
+  describe("addWolf", function(){
+
+    var board
+    var tileSheet
+
+    beforeEach(function(){
+      board = jasmine.createSpyObj(board, ['addChild'])
+      tileSheet = jasmine.createSpyObj('tileSheet', {
+        '_frameWidth': 2,
+        '_frameHeight': 2
+      })
+    })
+
+    it('sets the wolf x attribute', function(){
+      addWolf(board, wolf, 1, 1, 1)
+      expect(wolf.x).toEqual(72)
+    })
+
+    it('adds the wolf to the board', function(){
+      addWolf(board, wolf, 1, 1, 1)
+      expect(board.addChild).toHaveBeenCalledWith(wolf)
+    })
+  })
+
+  describe("Location Methods", function(){
+
+    var board
+    var tileSheet
+
+    beforeEach(function(){
+      board = jasmine.createSpyObj(board, ['addChild'])
+      tileSheet = jasmine.createSpyObj('tileSheet', {
+        '_frameWidth': 2,
+        '_frameHeight': 2
+      })
+      addWolf(board, wolf, 1, 1, 1)
+    })
+
+    it("whichColumn returns the column number of the dog", function(){
+      expect(whichColumn(wolf)).toEqual(1)
+    })
+
+    it("WhichRow returns the row number of the dog", function(){
+      expect(whichRow(wolf)).toEqual(1)
+    })
+  })
 })
