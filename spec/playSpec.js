f4b5d9cc25ffe2b0e770693a0d8c3e4ab87636de
Laura Chan <laurachan205@gmail.com>
2018-03-23 12:07:57 +0000

    Add playersheet test to playSpec.js

--- spec/playSpec.js
+++ spec/playSpec.js
@@ -2,4 +2,21 @@ describe('Play', function() {
   it('player is defined', function(){
     expect(player).toBeDefined()
   })
+
+  it('playerSheet is defined', function(){
+    expect(playerSheet).toBeDefined()
+  })
+
+  // it('addPlayer is defined', function(){
+  //   expect(addPlayer()).toBeDefined()
+  // })
+  // it('whichColumn is defined', function(){
+  //   expect(whichColumn()).toBeDefined()
+  // })
+  // it('whichRow is defined', function(){
+  //   expect(whichRow()).toBeDefined()
+  // })
+  // it('movePlayer is defined', function(){
+  //   expect(movePlayer()).toBeDefined()
+  // })
 })
