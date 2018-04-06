769c88598fb42b7afcb0762e695cc8f918046bd1
Laura Chan <laurachan205@gmail.com>
2018-04-06 21:49:40 +0100

    Add fade to about box

--- interface.js
+++ interface.js
@@ -3,6 +3,7 @@ $( document ).ready(function() {
   $('#button').click(function(e) {
     $('#about_us').css("background-color", "#ffffff8a");
     $('#about_us').text("This game was created by Laura, Justyna, Ben, Steve and Noel at week 9 of Makers Academy. Check out our GitHb repo to learn more about the project and copyrighted graphics & music: https://github.com/lwkchan/bombercat.")
+    $('#about_us').fadeOut(3000);
   });
 
 });
